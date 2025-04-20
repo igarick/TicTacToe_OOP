2ac45ee3a5787e588569323c9c66dd51f59b636d
Igor_home <keaerta@gmail>
2025-04-20 18:34:02 +0500

    paint map

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // --- абстрактный класс сущности
+        // makeMove()
+
+        // наследник Крестики
+        // наследник Нолики
+
+        // --- класс карта
+        // коллекции хранятся существа и их место на карте
+
+        // --- Simulation класс игровой процесс
+        // - карта
+        // - счетчик ходов
+        // - рендерер поля
+        // - Actions вызывает действия
+        // nextTurn()
+        // startSimulation
+
+        // --- Actions
+        // - вызыввает каждому makeMove()
+    }
+}
